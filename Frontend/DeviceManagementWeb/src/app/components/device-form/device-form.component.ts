ee5f3cb57c144e4f1383022ab817048527ead18b
george.rus <georgedanielrus0505@gmail.com>
2026-04-08 20:04:33 +0300

    added validation to create and edit device fields

--- Frontend/DeviceManagementWeb/src/app/components/device-form/device-form.component.ts
+++ Frontend/DeviceManagementWeb/src/app/components/device-form/device-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnInit, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Device } from '../../models/device.model';
 
 @Component({
@@ -13,16 +13,29 @@ import { Device } from '../../models/device.model';
 export class DeviceFormComponent implements OnInit, OnChanges {
   @Input() initialData: Device | null = null;
   @Input() isEditing = false;
+  @Input() existingDevices: Device[] = [];
   
   @Output() save = new EventEmitter<Device>();
   @Output() cancel = new EventEmitter<void>();
 
   deviceForm: FormGroup;
 
+  duplicateNameValidator = (control: AbstractControl): ValidationErrors | null => {
+    if (!control.value || !this.existingDevices) return null;
+    
+    const currentId = this.deviceForm?.get('id')?.value || 0;
+   
+    const exists = this.existingDevices.some(
+      d => d.name.toLowerCase() === control.value.toLowerCase() && d.id !== currentId
+    );
+
+    return exists ? { duplicateName: true } : null; 
+  };
+
   constructor(private fb: FormBuilder) {
     this.deviceForm = this.fb.group({
-      id: [0], // Default to 0
-      name: ['', Validators.required],
+      id: [0],
+      name: ['', [Validators.required, this.duplicateNameValidator]],
       manufacturer: ['', Validators.required],
       type: ['Laptop', Validators.required],
       operatingSystem: ['Windows', Validators.required],
@@ -31,8 +44,7 @@ export class DeviceFormComponent implements OnInit, OnChanges {
       ramAmountGb: [null, [Validators.required, Validators.min(1)]],
       status: ['Available', Validators.required],
       description: [''],
-      // ADD THESE SO WE DON'T LOSE THE OWNER WHEN EDITING!
-      assignedUserID: [null], 
+      assignedUserID: [null],
       assignedUserName: [null]
     });
   }
@@ -49,7 +61,6 @@ export class DeviceFormComponent implements OnInit, OnChanges {
     if (this.initialData) {
       this.deviceForm.patchValue(this.initialData); 
     } else {
-      // THE FIX: Explicitly set 'id: 0' so it doesn't become 'null'!
       this.deviceForm.reset({ 
         id: 0, 
         type: 'Laptop', 
