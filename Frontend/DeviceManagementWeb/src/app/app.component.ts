4fe6adc171b5f951f533a76f2f0196cfefe1ffb8
george.rus <georgedanielrus0505@gmail.com>
2026-04-08 23:03:29 +0300

    Finished all the functionalities of phase 2 and styling

--- Frontend/DeviceManagementWeb/src/app/app.component.ts
+++ Frontend/DeviceManagementWeb/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
